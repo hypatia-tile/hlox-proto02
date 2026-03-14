8f58f4482fec035312cb080fb514e1fbb9cd66ca
shinokun <hypatia.tile02021@gmail.com>
2026-03-14 16:54:07 +0900

    test: add simple test for practice
    - Not related to any content of main modules

--- hlox-cli/test/Main.hs
+++ hlox-cli/test/Main.hs
@@ -1,4 +1,31 @@
 module Main (main) where
 
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  runTestTTAndExit tests
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+
+test1 :: Test
+test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
+test2 :: Test
+test2 = TestCase (do (x,y) <- partA 3
+                     assertEqual "for the first result of partA," 5 x
+                     b <- partB y
+                     assertBool ("(partB " ++ show y ++ ") failed") b)
+
+
+foo :: Int -> (Int, Int)
+foo 3 = (1, 2)
+foo _ = undefined
+
+partA :: Int -> IO (Int, Int)
+partA 3 = return (5, 6)
+partA _ = undefined
+
+partB :: Int -> IO Bool
+partB 6 = return True
+partB _ = return False
