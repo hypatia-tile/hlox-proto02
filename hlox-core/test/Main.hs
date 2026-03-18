de5fb94ce3933e1c925d0c476f4b0b1140a1350c
shinokun <hypatia.tile02021@gmail.com>
2026-03-18 09:06:37 +0900

    test: add test for operator parsing
    Add test dependency in hlox-core.cabal
    and write a simple test to check that operators are parsed correctly.
    However, only the failure case is tested.

--- hlox-core/test/Main.hs
+++ hlox-core/test/Main.hs
@@ -1,4 +1,32 @@
 module Main (main) where
 
+import Control.Monad.State
+import Data.Lox.Lexer
+import Lox.Lexer
+import Lox.Lexer.Operator
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = runTestTTAndExit testOp1
+
+-- TODO: write test
+-- testOperator :: IO ()
+-- testOperator = do
+--   let f = flip (evalState . enumString) (Position 1 1)
+--   let g = print . runStateT parseOperator . f
+--   g "hello"
+--   g "="
+--   g ">"
+--   g ">=hello"
+--   g ">==hello"
+
+testOp1 :: Test
+testOp1 =
+  TestCase
+    ( do
+        let source = evalState (enumString "hello") (Position 1 1)
+        let result = runStateT parseOperator source
+        assertEqual "Should be Nothing" Nothing result
+    )
+
+-- TODO: write more test cases for operators
